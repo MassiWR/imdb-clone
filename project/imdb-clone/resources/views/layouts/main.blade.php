c01f0a0ff7cfb14328d0d7a1b3ef9ec09a2e68b5
Licia Thörn <liciathorn@gmail.com>
2022-04-05 08:59:20 +0200

    Minor styling on main blade

--- project/imdb-clone/resources/views/layouts/main.blade.php
+++ project/imdb-clone/resources/views/layouts/main.blade.php
@@ -26,18 +26,6 @@
                 </li>
 
                 <li class="md:ml-6 mt-3 md:mt-0">
-                    <a href="#" class="hover:text-gray-300">Tv Shows</a>
-                </li>
-
-                <li class="md:ml-6 mt-3 md:mt-0">
-                    <a href="#" class="hover:text-gray-300">Actors</a>
-                </li>
-
-                <li class="md:ml-6 mt-3 md:mt-0">
-                    <a href="#" class="hover:text-gray-300">Genres</a>
-                </li>
-
-                <li class="md:ml-6 mt-3 md:mt-0">
                     <a href="#" class="hover:text-gray-300">Watchlist</a>
                 </li>
             </ul>
