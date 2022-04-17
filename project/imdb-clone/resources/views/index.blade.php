d8c2cd9d57195bc80a6f9d5ad192a70c7fea3c4a
Licia Thörn <liciathorn@gmail.com>
2022-04-17 13:26:05 +0200

    Style index blade

--- project/imdb-clone/resources/views/index.blade.php
+++ project/imdb-clone/resources/views/index.blade.php
@@ -1,20 +1,21 @@
 @extends('layouts.main')
 
 @section('content')
-   <section class="container mx-auto px-4 pt-16">
+<section class="container mx-auto px-4 pt-16">
     <!-- popular movies section -->
     <div class="popular-movies">
-        <h2 class="text-lg text-center uppercase tracking-wider text-yellow-300 font-semibold pb-4">Most Popular Movies</h2>
+        <h2 class="text-4xl text-center uppercase tracking-wider text-orange-300 font-semibold pb-4">Popular Movies</h2>
 
         <!-- Grid for all movies -->
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 lg:grid-8 gap-10">
-           @foreach ($popularMovies as $movie)
-            @if ($loop->index < 12)
-            <x-movie-card :movie="$movie" :genres="$genres"/>
+            @foreach ($popularMovies as $movie)
+            @if ($loop->index
+            < 12) <x-movie-card :movie="$movie" :genres="$genres" />
             @endif
             @endforeach
         </div>
     </div>
-   </section>
-@endsection
+    
+</section>
 
+@endsection
