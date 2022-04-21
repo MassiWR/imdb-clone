d6e6d15234fea502453fac0358c4f21cca0b5d90
Licia Thörn <liciathorn@gmail.com>
2022-04-21 16:27:20 +0200

    last styling menuMovies

--- project/imdb-clone/resources/views/menuMovies.blade.php
+++ project/imdb-clone/resources/views/menuMovies.blade.php
@@ -1,38 +1,41 @@
 @extends('layouts.main')
 @section('content')
 
-<section class="container mx-auto px-4 pt-16 flex justify-evenly">
+<section class="container mx-auto px-4 pt-16">
 
     <!--Grid for trending movies -->
-    <div class="trendingMovies">
+    <div class="trendingMovies pb-10">
         <h2 class="text-4xl text-center uppercase tracking-wider text-orange-300 font-semibold pb-4">Trending Movies</h2>
-        <div class="container mx-auto px-4 py-10 flex flex-col md:flex-row gap-10 ">
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 lg:grid-8 gap-10">
+        
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-10">
                 @foreach ($trendingMovies as $movie)
                 @if ($loop->index < 5) <a href="{{ route('movies.show', $movie['id']) }}">
                     {{$movie['title']}}
                     <img src="{{'https://image.tmdb.org/t/p/w185/'.$movie['poster_path']}}" alt="Poster" class="hover:opacity-90">
                     </a>
-                    @endif
-                    @endforeach
+                @endif
+                @endforeach
 
             </div>
         </div>
+    </div>
 
         <!--Grid for top rated movies -->
+        <div class="topRated mt-10 mb-10">
         <h2 class="text-4xl text-center uppercase tracking-wider text-orange-300 font-semibold pb-4">Top Rated</h2>
-        <div class="container mx-auto px-4 py-10 flex flex-col md:flex-row gap-10 ">
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 lg:grid-8 gap-10">
+        
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-10">
                 @foreach ($topRated as $movie)
                 @if ($loop->index < 5) <a href="{{ route('movies.show', $movie['id']) }}">
-                    {{$movie['title']}}</h2>
+                    {{$movie['title']}}
                     <img src="{{'https://image.tmdb.org/t/p/w185/'.$movie['poster_path']}}" alt="Poster" class="hover:opacity-90">
                     </a>
-                    @endif
-                    @endforeach
+                @endif
+                @endforeach
 
             </div>
         </div>
+</div>
 
 
 </section>
