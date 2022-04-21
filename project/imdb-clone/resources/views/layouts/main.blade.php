d1098f245e7832adfe3437032673ac17950fd861
Licia Thörn <liciathorn@gmail.com>
2022-04-21 16:06:38 +0200

    Style to moviesMenu

--- project/imdb-clone/resources/views/layouts/main.blade.php
+++ project/imdb-clone/resources/views/layouts/main.blade.php
@@ -27,7 +27,7 @@
 
             </ul>
 
-            <!-- Search bar?? -->
+
             <ul class="flex flex-col md:flex-row mt-3 md:mt-0 items-center">
                 <div class="relative mt-3 mr-4 md:mt-0 hover:text-gray-300">
                     <a class="hover:text-orange-500" href="{{ route('auth.login')}}">Login</a>
--- project/imdb-clone/resources/views/menuMovies.blade.php
+++ project/imdb-clone/resources/views/menuMovies.blade.php
@@ -1,45 +1,41 @@
-
 @extends('layouts.main')
 @section('content')
 
-<section class="container mx-auto pt-16">
+<section class="container mx-auto px-4 pt-16 flex justify-evenly">
 
-<!--Grid for trending movies -->
-<div class="trendingMovies">
-<h2 class="text-4xl text-left uppercase tracking-wider text-orange-300 font-semibold pb-4">Trending Movies</h2>
-<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 lg:grid-8 gap-10">
-            @foreach ($trendingMovies as $movie)
-            @if ($loop->index < 5)
-                {{$movie['title']}}
-            <div class="mt-4"> <!--fix position of poster/name !-->
-    <a href="{{ route('movies.show', $movie['id']) }}">
-        <img src="{{'https://image.tmdb.org/t/p/w185/'.$movie['poster_path']}}" alt="Poster" class="hover:opacity-90">
-    </a>
-            @endif
-            @endforeach
-            
+    <!--Grid for trending movies -->
+    <div class="trendingMovies">
+        <h2 class="text-4xl text-center uppercase tracking-wider text-orange-300 font-semibold pb-4">Trending Movies</h2>
+        <div class="container mx-auto px-4 py-10 flex flex-col md:flex-row gap-10 ">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 lg:grid-8 gap-10">
+                @foreach ($trendingMovies as $movie)
+                @if ($loop->index < 5) <a href="{{ route('movies.show', $movie['id']) }}">
+                    {{$movie['title']}}
+                    <img src="{{'https://image.tmdb.org/t/p/w185/'.$movie['poster_path']}}" alt="Poster" class="hover:opacity-90">
+                    </a>
+                    @endif
+                    @endforeach
+
+            </div>
         </div>
-</div>
 
+        <!--Grid for top rated movies -->
+        <h2 class="text-4xl text-center uppercase tracking-wider text-orange-300 font-semibold pb-4">Top Rated</h2>
+        <div class="container mx-auto px-4 py-10 flex flex-col md:flex-row gap-10 ">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 lg:grid-8 gap-10">
+                @foreach ($topRated as $movie)
+                @if ($loop->index < 5) <a href="{{ route('movies.show', $movie['id']) }}">
+                    {{$movie['title']}}</h2>
+                    <img src="{{'https://image.tmdb.org/t/p/w185/'.$movie['poster_path']}}" alt="Poster" class="hover:opacity-90">
+                    </a>
+                    @endif
+                    @endforeach
 
-<!--Grid for top rated movies -->
-<div class="topRated">
-<h2 class="text-4xl text-left uppercase tracking-wider text-orange-300 font-semibold pb-4">Top Rated</h2>
-<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-6 lg:grid-8 gap-10">
-            @foreach ($topRated as $movie)
-            @if ($loop->index < 5)
-                {{$movie['title']}}
-                <div class="mt-4"> <!--fix position of poster/name !-->
-    <a href="{{ route('movies.show', $movie['id']) }}">
-        <img src="{{'https://image.tmdb.org/t/p/w185/'.$movie['poster_path']}}" alt="Poster" class="hover:opacity-90">
-    </a>
-            @endif
-            @endforeach
+            </div>
         </div>
-</div>
 
 
 </section>
 
-    
+
 @endsection
