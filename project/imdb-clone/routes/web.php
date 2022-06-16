0a572928c98217042fbd75c31bb8f918f5f5bb06
MassiWR <90767729+MassiWR@users.noreply.github.com>
2022-06-16 10:24:30 +0200

    Remove unused routes

--- project/imdb-clone/routes/web.php
+++ project/imdb-clone/routes/web.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MoviesController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MenuMoviesController; 
+use App\Http\Controllers\MenuMoviesController;
+
 
 
 /* |-------------------------------------------------------------------------- | Web Routes |-------------------------------------------------------------------------- | | Here is where you can register web routes for your application. These | routes are loaded by the RouteServiceProvider within a group which | contains the "web" middleware group. Now create something great! | */
@@ -22,8 +23,5 @@ Route::group(['middleware' => ['AuthCheck']], function () {
     Route::get('/profile', [AuthController::class , 'profile']);
     Route::get('/login', [AuthController::class , 'login'])->name('auth.login');
     Route::get('/register', [AuthController::class , 'register'])->name('auth.register');
-    Route::get('/staff', [AuthController::class , 'staff'])->name('auth.staff');
-    Route::get('/settings', [AuthController::class , 'settings'])->name('auth.settings');
-
 
 });
