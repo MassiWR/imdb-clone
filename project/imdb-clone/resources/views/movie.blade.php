30b5c34814885c0d61c15e1e273bfefb118f1494
Licia Thörn <liciathorn@gmail.com>
2022-06-17 13:05:41 +0200

    Take away log out button from different blades.

--- project/imdb-clone/resources/views/movie.blade.php
+++ project/imdb-clone/resources/views/movie.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
-@extends('layouts.logged')
+
 
 @section('content')
     <!-- info about a specific movie -->
@@ -40,14 +40,17 @@
                                 
                                 
                             </div>
-                               
+                                                                                       
                            @endif
                         @endforeach
                         </div>
                     
                     </div>
-                     
-                <div>
+
+                    <div class="mt-10">
+                    <button class="flex items-center bg-gray-500 text-white rounded-sm font-semibold px-6 py-3 hover:bg-orange-600" type="submit">Add to Watchlist</button>
+                </div>
+
             </div>
         </div>
     </section> <!-- end of movie info -->
